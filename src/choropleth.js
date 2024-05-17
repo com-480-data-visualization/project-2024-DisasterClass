fd9f92223786ac8388965a11c19fa2edd15d69ca
AfroDeivid <92383275+AfroDeivid@users.noreply.github.com>
2024-05-17 17:50:11 +0200

    first draft of chloropeth

--- src/choropleth.js
+++ src/choropleth.js
@@ -1,77 +1,108 @@
 class World_Map {
+
     constructor(svg_element_id) {
-
-        this.svg = d3.select('#' + svg_element_id);
-
-		const svg_viewbox = this.svg.node().viewBox.animVal;
-		this.svg_width = svg_viewbox.width;
-		this.svg_height = svg_viewbox.height;
-
-        // Fit the projection.
-        this.projection = d3.geoEqualEarth().fitExtent([[2, this.marginTop + 2], [this.width - 2, this.height]], { type: "Sphere" });
+        this.svg = d3.select(`#${svg_element_id}`);
+        const svg_viewbox = this.svg.node().viewBox.animVal;
+        this.svg_width = svg_viewbox.width || 960;
+        this.svg_height = svg_viewbox.height || 500;
+        
+        this.projection = d3.geoEqualEarth()
+                                .fitExtent([[2, 2], [this.svg_width - 2, this.svg_height]], {type: "Sphere"});
         this.path = d3.geoPath(this.projection);
 
-        // Index the values and create the color scale.
-        this.valuemap = new Map(hale.map(d => [d.name, d.hale]));
-        this.color = d3.scaleSequential(d3.extent(this.valuemap.values()), d3.interpolateYlGnBu);
+        const data_promise = d3.csv("declaration_per_country.csv").then(data => {
+            data = new Map(data.map(d => [d.Country, +d.Percentage]));
+            console.log("Data Map:", data);
+            return data;
+        });
 
-        // Append the legend.
-        this.svg.append("g")
-            .attr("transform", "translate(20,0)")
-            .append(() => Legend(this.color, { title: "Healthy life expectancy (years)", width: 260 }));
+        const map_promise = d3.json("countries-50m.json");
 
-        // Add a white sphere with a black border.
-        this.svg.append("path")
-            .datum({ type: "Sphere" })
-            .attr("fill", "white")
-            .attr("stroke", "currentColor")
-            .attr("d", this.path);
+        Promise.all([data_promise, map_promise]).then(values => {
+            this.dataMap = values[0];
+            this.map = values[1];
 
-        // Add a path for each country and color it according to this data.
-        this.svg.append("g")
-            .selectAll("path")
-            .data(countries.features)
-            .join("path")
-            .attr("fill", d => this.color(this.valuemap.get(d.properties.name)))
+            console.log("Value of this.map:", this.map); // Add logging
+
+            this.drawMap();
+        });
+    }
+    drawMap() {
+        const world = this.map; // Assign the loaded JSON data directly
+    
+        const countries = topojson.feature(world, world.objects.countries).features;
+
+        const colorScale = d3.scaleSequentialLog(d3.interpolateYlGnBu)
+            .domain([1, d3.max(Array.from(this.dataMap.values()))]);
+    
+        this.svg.selectAll("path")
+            .data(countries)
+            .enter().append("path")
             .attr("d", this.path)
+            .attr("fill", d => {
+                const percentage = this.dataMap.get(d.properties.name) ;
+                if (percentage === undefined) {
+                    return "black"; // No information available
+                } else if (percentage === 0) {
+                    return "#ccc"; // Percentage is zero
+                } else {
+                    return colorScale(percentage); // Color based on percentage
+                }
+            })
+            .attr("stroke", "#333")
+            .attr("stroke-width", 0.5)
             .append("title")
-            .text(d => `${d.properties.name}\n${this.valuemap.get(d.properties.name)}`);
+            .text(d => { const percentage = this.dataMap.get(d.properties.name) ;
+                return `${d.properties.name}\n${percentage !== undefined ? percentage + "%" : "No data"}`;
+            });
+            
+        // Create legend
+        const legendWidth = 300;
+        const legendHeight = 10;
+        const maxPercentage = d3.max(Array.from(this.dataMap.values()));
+        const numTicks = Math.min(Math.floor(maxPercentage/10), 10); 
 
-        // Add a white mesh.
-        this.svg.append("path")
-            .datum(countrymesh)
-            .attr("fill", "none")
-            .attr("stroke", "white")
-            .attr("d", this.path);
-
-        return this.svg.node();
+        function logspace(start, end, num) {
+            const scale = (Math.log(end) - Math.log(start)) / (num - 1);
+            return Array.from({length: num}, (_, i) => Math.exp(Math.log(start) + (i * scale)));
+        }
+    
+        const x = d3.scaleLog()
+            .domain([1, maxPercentage])
+            .range([0, legendWidth]);
+    
+        const xAxis = d3.axisBottom(x)
+            .tickValues(logspace(1, maxPercentage,numTicks)) 
+            .tickFormat(d3.format(".0f"));
+    
+        const legend = this.svg.append("g")
+            .attr("class", "legend")
+            .attr("transform", `translate(${this.svg_width - legendWidth - 20}, ${this.svg_height - 30})`);
+    
+        legend.append("g")
+            .selectAll("rect")
+            .data(d3.range(legendWidth))
+            .enter().append("rect")
+            .attr("x", d => d)
+            .attr("y", 0)
+            .attr("width", 1)
+            .attr("height", legendHeight)
+            .attr("fill", d => colorScale(x.invert(d)));
+    
+        legend.append("g")
+            .attr("transform", `translate(0, ${legendHeight})`)
+            .call(xAxis);
     }
 }
 
 function whenDocumentLoaded(action) {
-if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", action);
-} else {
-    // `DOMContentLoaded` already fired
-    action();
-}
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", action);
+    } else {
+        action();
+    }
 }
 
 whenDocumentLoaded(() => {
-	plot_object = new World_Map('choropleth_map');
+    new World_Map('choropleth_map');
 });
-
-/*Plot.plot({
-projection: "equal-earth",
-width: 928,
-height: 928 / 2,
-color: {scheme: "YlGnBu", unknown: "#ccc", label: "Healthy life expectancy (years)", legend: true},
-marks: [
-    Plot.sphere({fill: "white", stroke: "currentColor"}),
-    Plot.geo(countries, {
-    fill: (map => d => map.get(d.properties.name))(new Map(hale.map(d => [d.name, d.hale]))),
-    }),
-    Plot.geo(countrymesh, {stroke: "white"}),
-]
-})
-*/
